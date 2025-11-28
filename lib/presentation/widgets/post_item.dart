d4a4a4c5f443bf8eba388341d1bd7df49a85cce0
Fahad TT <ttfahad2317@gmail.com>
2025-11-28 11:37:37 +0530

    post item widget created

---
+++ lib/presentation/widgets/post_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../../data/models/post_model.dart';
+
+class PostItem extends StatelessWidget {
+  final Post post;
+
+  const PostItem({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              post.title,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              post.body,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[700],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
